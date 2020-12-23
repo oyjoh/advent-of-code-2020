e4c23dadf5d7303811e29294af171ffac8240ace
oyjoh <oyvind.bre.joh@gmail.com>
2020-12-23 02:07:05 +0100

    day21

--- day21/day21.py
+++ day21/day21.py
@@ -1,53 +1,67 @@
 def solve_part_one(data):
     master_dict = {}
-    master_igr = []
+    master_ingredients = []
 
     for line in data:
         allergens = line[line.find('(') + 10: line.find(')')].split(', ')
         ingredients = line[:line.find('(')].split()
-        master_igr += ingredients
+        master_ingredients += ingredients
 
         for a in allergens:
             if a not in master_dict.keys():
                 master_dict[a] = []
             master_dict[a].append(set(ingredients.copy()))
 
-        # print(ingredients, end=' --> ')
-        # print(allergens)
-
     core = set([])
 
     for val in master_dict.values():
-        allger_set = set.intersection(*val)
-        for val in allger_set:
-            core.add(val)
+        allergens_set = set.intersection(*val)
+        for v in allergens_set:
+            core.add(v)
 
     counter = 0
-    for i in master_igr:
+
+    for i in master_ingredients:
         if i not in core:
             counter += 1
-    return counter
+    return counter, master_dict
+
+
+def remove_val_dict(param, new_dict):
+    for key in new_dict:
+        if param in new_dict[key]:
+            new_dict[key].remove(param)
+    return new_dict
+
+
+def solve_part_two(master_dict):
+    for key in master_dict:
+        master_dict[key] = set.intersection(*master_dict[key])
+    new_dict = dict(sorted(master_dict.items(), key=lambda item: len(item[1])))
+
+    sol = {}
+    while True:
+        if len(new_dict) == 0:
+            break
+        for key in new_dict:
+            if len(new_dict[key]) == 1:
+                sol[key] = new_dict.pop(key).pop()
+                new_dict = remove_val_dict(sol[key], new_dict)
+                break
+
+    result = []
+    for key in sorted(sol.keys()):
+        result.append(sol[key])
+
+    return ','.join(result)
 
 
 def main():
     data = open('day21.txt', 'r').read().splitlines()
 
-    print(f'Part One: {solve_part_one(data)}')
-
-
-def demo():
-    x = {1, 2, 3}
-    y = {0, 2, 3}
-    k = {2, 0, 4}
-    l = [x, y, k]
-    z = x.intersection(y)
-    sd = set.intersection(*l)
-    # a = x.symmetric_difference(sam)
-
-    ee = (x ^ y ^ k)
-    print(sd)
-    # print(ee)
-    # print(a)
+    part_one_ans, master_dict = solve_part_one(data)
+    print(f'Part One: {part_one_ans}')
+    print(f'Part Two: {solve_part_two(master_dict)}')
 
 
 if __name__ == '__main__':
